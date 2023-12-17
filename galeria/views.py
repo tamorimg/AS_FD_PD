a459c034279727d5330a81584dfab3e32ec1961e
tamorimg <thiago.suportejf@gmail.com>
2023-12-17 20:45:12 -0300

    02. Admin | Ajuste função index em views.py

--- galeria/views.py
+++ galeria/views.py
@@ -5,7 +5,7 @@ from galeria.models import Fotografia
 def index(request):
     fotografias = Fotografia.obhects.all()
 
-    return render(request, 'galeria/index.html', {"cards": dados})
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
