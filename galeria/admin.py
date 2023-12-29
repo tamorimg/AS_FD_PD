02051b4fddf809c98af52a413ce608db0b47ee2f
tamorimg <thiago.suportejf@gmail.com>
2023-12-29 07:39:42 -0300

    02. Admin | 07.Incluindo categoria

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from galeria.models import Fotografia
 
-admin.site.register(Fotografia)
+class ListandoFotografias(admin.ModelAdmin):
+    list_display=("id", "nome", "legenda")
+    list_display_links=("id", "nome")
+    search_fields=("nome",)
+
+admin.site.register(Fotografia, ListandoFotografias)
+
+
